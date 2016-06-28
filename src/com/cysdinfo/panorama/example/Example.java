79ffe37640d71a54ce791609937fe4546a456e2f
wk <wangkun5310@gmail.com>
2016-06-28 16:03:42 +0800

    项目构建

---
+++ src/com/cysdinfo/panorama/example/Example.java
@@ -0,0 +1,20 @@
+package com.cysdinfo.panorama.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	@RequestMapping("/hello")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+
+}
